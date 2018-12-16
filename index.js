1f52b950c5be8c453522b8715c7ac83222a642f0
ig.kos <ig.fire@gmail.com>
2018-12-16 20:04:10 +0200

    fix Code repository and Web app

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/igkostyuk/a-tiny-JS-world/blob/master/index.js
+   Web app: https://igkostyuk.github.io/a-tiny-JS-world/
    */
 
 
